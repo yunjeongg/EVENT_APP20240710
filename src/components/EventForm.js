7de60a9eb0ec7e1c199cb91d50dc191fb9413f57
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-16 11:21:59 +0900

    # 020. event 창 요청시 토큰 포함하기

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './EventForm.module.scss';
 import { useNavigate, Form, redirect } from 'react-router-dom';
 import { EVENT_URL } from '../config/host-config';
+import { getUserToken } from '../config/auth';
 
 const EventForm = ({ method, event = {} }) => {
   const {
@@ -172,6 +173,7 @@ export const action = async ({ request, params }) => {
     method: request.method,
     headers: {
       'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + getUserToken()
     },
     body: JSON.stringify(payload),
   });
