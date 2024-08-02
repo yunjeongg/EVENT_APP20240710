837680b4b3b2500caefd8f3a0d24b514da6deb39
yunjeongg <bae.yunjeongg@gmail.com>
2024-08-02 11:19:27 +0900

    로그인제목변경

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -11,7 +11,7 @@ const LoginForm = () => {
   return (
     <>
       <Form method="post" className={styles.form}>
-        <h1>자동배포 로그인</h1>
+        <h1>내가산도메인 로그인</h1>
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required/>
